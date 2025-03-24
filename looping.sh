0e0d1996e668ed8deaa40fd0ff0dc5906f98a65d
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-03-24 17:28:23 +0530

    Create looping.sh

---
+++ looping.sh
@@ -0,0 +1,12 @@
+###If Else loop
+
+read -p "enter the nnumber 1" number_one
+read -p "enter the number 2" number_two
+
+if [ "number_one" = "number_two"]; then
+      echo "number are same"
+
+else
+     echo "number are different"
+
+fi
