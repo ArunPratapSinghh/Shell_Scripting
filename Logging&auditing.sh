845b6d3fd3a0fcd09be7361db2de20254a035e9b
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-05-19 02:23:39 +0530

    Update Logging&auditing.sh

--- Logging&auditing.sh
+++ Logging&auditing.sh
@@ -4,7 +4,7 @@
 echo "Display messages in kernel ring buffer"
 dmesg
 
-echo "Display logs stored in the systemd journal"
+echo "###Display logs stored in the systemd journal"
 journalctl
 
 echo "Display logs for a specific unit (service)"
