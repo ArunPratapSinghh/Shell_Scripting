f6bee99ef90dc637d71707ba5f66337720b6c38a
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-06-23 14:22:25 +0200

    Update looping.sh

--- looping.sh
+++ looping.sh
@@ -3,7 +3,7 @@
 read -p "enter the nnumber 1" number_one
 read -p "enter the number 2" number_two
 
-if [ "number_one" = "number_two"]; then
+if [ "$number_one" = "$number_two"]; then
       echo "number are same"
 
 else
