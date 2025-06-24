0b95bae193e531b01d0b8ccb268caccaa535dee8
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-06-24 12:42:59 +0200

    Update disk_details.sh
    full disk
    specific disk"/dev/sda1"

--- disk_details.sh
+++ disk_details.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+
+echo "________________disk usage details________________"
+
+Details_list="Backup_disk_Detials.txt"
+echo "Full disk details"
+df -h >> $Details_list
+
+echo "/dev/sda1 disk usage"
+df -h |grep "/dev/sda1"|awk "{print $5}" >> $Details_list
+
+echo "______________Disk script completed________________"
